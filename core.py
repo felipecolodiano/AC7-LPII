ff04e7611ce4051db3884dbb6479e44d5a97df9b
Felipe Colodiano <42190748+felipecolodiano@users.noreply.github.com>
2018-10-13 18:23:29 -0300

    Create core.py

---
+++ core.py
@@ -0,0 +1,17 @@
+print('''
+############################################################  
+#  Bem-vindo(a) ao sistema da vacinação Impacta-Nozes      #
+#                                                          #
+#     Animais que poderão receber vacinas:                 #
+#        - Gato                                            #
+#        - Cachorro                                        #
+#        - Cavalo                                          #
+#                                                          #  
+#     Vacinas disponíveis:                                 #
+#        - Intra Venosa (Cachorro)                         #
+#        - Intra Muscular (Gato)                           # 
+#        - Superficie (Cavalo)                             #
+#                                                          #
+#               Digite 1 para iniciar                      #              
+############################################################
+''')
