5acb4136ed05780502b6a7d7ce5ff88971ea5b26
Douglas Barbosa <42191046+douglasbarbosa90@users.noreply.github.com>
2018-10-13 19:05:46 -0300

    Update core.py
    Falta tratar as opções...

--- core.py
+++ core.py
@@ -1,17 +1,12 @@
-print('''
-############################################################  
-#  Bem-vindo(a) ao sistema da vacinação Impacta-Nozes      #
-#                                                          #
-#     Animais que poderão receber vacinas:                 #
-#        - Gato                                            #
-#        - Cachorro                                        #
-#        - Cavalo                                          #
-#                                                          #  
-#     Vacinas disponíveis:                                 #
-#        - Intra Venosa (Cachorro)                         #
-#        - Intra Muscular (Gato)                           # 
-#        - Superficie (Cavalo)                             #
-#                                                          #
-#               Digite 1 para iniciar                      #              
-############################################################
-''')
+from Business import *
+#from Business.animals import Animal, Gato, Cachorro, Cavalo
+#from Business.vaccine import Vaccine, Intra_muscular, Intra_venosa, Superficie
+
+def main():
+    print("Bem-vindo ao controle de Zoonoses de São Paulo")
+    print("Escolha uma das opções abaixo para vacinação: ")
+    print('1 - Cachorro')
+    print('2 - Cavalo')
+    print('3 - Gato')
+    
+    opt = int(input('DIGITE O N° DA OPERAÇÃO DESEJADA:>>>>'))
