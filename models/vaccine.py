b733bf00ed2eabead25f5473e832f192f7152788
Douglas Barbosa <42191046+douglasbarbosa90@users.noreply.github.com>
2018-10-13 19:08:09 -0300

    Create vaccine.py

---
+++ models/vaccine.py
@@ -0,0 +1,37 @@
+from .animals import Animal, Gato, Cachorro, Cavalo
+from ..Exceptions.exceptions import TipoVacinaInvalido
+
+class Vaccine():
+    def __init__(self,tipo,dose):
+        self.tipo = tipo
+        self.dose = dose
+
+    def vacinar(self):
+        #try:
+           # if Gato == 
+        return "Vacinando de forma GENERICA"
+
+#vacina Gato 
+class Intra_muscular(Vaccine):
+    def vacinar(self):
+        return f'Vacina: {self.tipo}, Dose: {self.dose}ml'
+
+    def __str__(self):
+        return "Aplicando..."
+
+
+#vacina Cachorro
+class Intra_venosa(Vaccine):
+    def vacinar(self):
+        return f'Vacina: {self.tipo}, Dose: {self.dose}ml'
+        
+    def __str__(self):
+        return "Aplicando..."
+
+#vacina Cavalo
+class Superficie(Vaccine):
+    def vacinar(self):
+        return f'Vacina: {self.tipo}, Dose: {self.dose}ml'
+
+    def __str__(self):
+        return "Aplicando..."
