fca98c72c5e8b5ccc0a2e520dfea9ce97a5d391f
Douglas Barbosa <42191046+douglasbarbosa90@users.noreply.github.com>
2018-10-14 14:57:02 -0300

    Update core.py
    Implementando mais opções

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
-from Business import *
-#from Business.animals import Animal, Gato, Cachorro, Cavalo
-#from Business.vaccine import Vaccine, Intra_muscular, Intra_venosa, Superficie
+#from Business import *
+from Business.animals import Animal, Gato, Cachorro, Cavalo
+from Business.vaccine import Vaccine, Intra_muscular, Intra_venosa, Superficie
 
 def main():
     print("Bem-vindo ao controle de Zoonoses de São Paulo")
@@ -9,4 +9,22 @@ def main():
     print('2 - Cavalo')
     print('3 - Gato')
     
-    opt = int(input('DIGITE O N° DA OPERAÇÃO DESEJADA:>>>>'))
+    optAnimal = int(input('DIGITE O N° DA OPERAÇÃO DESEJADA:>>>>'))
+
+    print("Escolha uma das opções do tipo de vacina: ")
+    print('1 - Intra-Venosa')
+    print('2 - Superficie')
+    print('3 - Intra-Muscular')
+
+    optVaccine = int(input('DIGITE O N° DA OPERAÇÃO DESEJADA:>>>>'))
+
+
+    if optAnimal == 1 and optVaccine == 1:
+        return Vaccine().Intra_venosa(animal, vacina)
+    elif optAnimal == 2 and optVaccine == 2:
+        return Vaccine().Superficie(animal, vacina)
+    elif optAnimal == 3 and optVaccine == 3:
+        return Vaccine().Intra_muscular(animal, vacina)
+    else:
+        print('OPIÇÃO INVÁLIDA!!!')
+        return main()
